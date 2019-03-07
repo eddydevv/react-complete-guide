f153fdac69ee9f9e1f04a5c9e51e6b8a891719b1
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-07 11:29:09 -0700

    Illustrate how to use state and props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: "Edwin",
+      age: "27"
+    }
+
+    this.inputNameHandler = this.inputNameHandler.bind(this)
+  }
+
+  inputNameHandler = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <Person />
+        <Person name={this.state.name} age={this.state.age} onChange={this.inputNameHandler} />
       </div>
     );
   }
