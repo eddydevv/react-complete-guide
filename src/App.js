a0476b0248d931a7810c7ecc4db7e685a33ee909
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-10 16:00:07 -0600

    Create togglePersonHandler to illustrate ternary operator

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ class App extends Component {
         { name: 'Stephanie', age: 26}
       ],
       otherState: 'some other value',
-      showPersons: true
+      showPersons: false
     }
 
     this.switchNameHandler = this.switchNameHandler.bind(this)
     this.nameChangeHandler = this.nameChangeHandler.bind(this)
+    this.togglePersonHandler = this.togglePersonHandler.bind(this)
   }
 
   switchNameHandler = (newName) => {
@@ -40,6 +41,11 @@ class App extends Component {
     })
   }
 
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -55,19 +61,20 @@ class App extends Component {
         <p>This is really working!</p>
         <button 
           style={style}
-          onClick={() => this.switchNameHandler('Max')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          onClick={()=> this.switchNameHandler('Maximilian')}
-          changed={this.nameChangeHandler}
-          >My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          onClick={this.togglePersonHandler}>Switch Name</button>
+        {this.state.showPersons ? <div><Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            onClick={()=> this.switchNameHandler('Maximilian')}
+            changed={this.nameChangeHandler}
+            >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div> : null}
       </div>
     );
   }
