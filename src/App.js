b6ad9f3f50492303d7461a924dcb14e9b5902b92
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-11 13:40:11 -0600

    Add Radium and include pseudo-selectors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import Person from './Person/Person'
+import Radium from 'radium'
 
 class App extends Component {
   constructor(props) {
@@ -52,11 +53,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen', 
+        color: 'black'
+      }
     }
 
     let persons = null
@@ -74,12 +80,26 @@ class App extends Component {
           })}
         </div>
       )
+
+      style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+    }
+
+    let classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style={style}
           onClick={this.togglePersonHandler}>Toggle Persons</button>
@@ -89,4 +109,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
