d252427ceab16893f2e279c456a5c70f827b4a2e
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-11 14:09:01 -0600

    Explore more of radium

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import Person from './Person/Person'
-import Radium from 'radium'
+import Radium, { StyleRoot } from 'radium'
 
 class App extends Component {
   constructor(props) {
@@ -53,6 +53,7 @@ class App extends Component {
 
   render() {
     const style = {
+      textAlign: 'center',
       backgroundColor: 'green',
       color: 'white',
       font: 'inherit',
@@ -97,14 +98,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
