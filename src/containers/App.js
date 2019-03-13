21e6bcc5f4b5c1d19d83ad2a27018e53cbabe2a8
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-13 15:40:50 -0600

    Introduce lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,8 @@ class App extends Component {
   constructor(props) {
     super(props)
 
+    console.log('[App.js] constructor')
+
     this.state = {
       persons: [
         { id: 'asdasd', name: 'Max', age: 28 },
@@ -20,6 +22,15 @@ class App extends Component {
     this.deletePersonHandler = this.deletePersonHandler.bind(this)
     this.nameChangeHandler = this.nameChangeHandler.bind(this)
     this.togglePersonHandler = this.togglePersonHandler.bind(this)
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state;
+  }
+  
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
   }
 
   nameChangeHandler = ( event, id ) => {
@@ -52,6 +63,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render')
 
     let persons = null
 
