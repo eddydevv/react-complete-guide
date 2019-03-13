5a057ae6328a6ed91b02adba242f5aa13aa07d17
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-13 15:59:47 -0600

    Add update-lifecycle hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import styles from './Person.module.css'
+import classes from './Person.module.css'
 
-const person = (props) => {
-  console.log('[Person.js] rendering...')
-  return (
-    <div className={styles.Person}>
-      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} />
-    </div>
-  )
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...')
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+        <p>{this.children}</p>
+        <input type="text" onChange={this.props.changed} />
+      </div>
+    )
+  }
 }
 
-export default person
+export default Person
