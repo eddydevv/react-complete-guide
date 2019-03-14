a641ed837e09b5aa67ce958b60b01730af27a8bc
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-14 15:58:42 -0600

    Add useEffect and componentWillUnmount life cycle hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './Cockpit.module.css'
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect')
+
+    setTimeout(() => {
+      alert('Saved data to cloud!')
+    }, 1000)
+    return () => {
+      return console.log('[Cockpit.js] cleanup work in useEffect!')
+    }
+  }, [])
 
   const assignedClasses = []
   let btnClass = ''
